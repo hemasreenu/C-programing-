b538338f991e7b5d5f363bc5e1bfc62b532d8025
hemasreenu <25kq1a5435pace.ac.in@gmail.com>
2025-11-25 09:26:59 +0530

    Add files via upload

---
+++ conditional operator two .c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+      int a;
+      a=100;
+      a%2==0?printf("even"):printf("odd");
+    return 0;
+}
+                                                                                                            
+
+
+
+
+
+
+
+
+
+
+
+
+    
---
+++ statement switch.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+int main()
+{
+    char ch;
+    printf("enter Char value:");
+    scanf("%c",&ch);
+    switch (ch){
+          case 'a':
+              printf("given is vowel");
+              break;
+          case 'e':
+              printf ("given is vowel");
+              break;
+          case 'i':
+              printf("given is vowel");
+              break;
+          case 'o':
+              printf("given is vowel");
+              break;
+          case 'u':
+              printf("given is vowel");
+              break;
+          default :
+              printf("given is consonant");
+                  
+    }                    
+       
+    
+    return 0;
+}
+                                                                                                            
+
+
+
+
+
+
+
+
+
+
+
+
+    
